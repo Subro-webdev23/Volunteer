06b06b5058d7649f2374511799cd40642949c612
Subro-webdev23 <subrota.webdev23@gmail.com>
2025-07-02 09:23:07 +0600

    some update on footer

--- src/Component/HeaderFooter/Footer.jsx
+++ src/Component/HeaderFooter/Footer.jsx
@@ -4,26 +4,28 @@ import { Link } from 'react-router';
 
 const Footer = () => {
     return (
-        <footer className="footer sm:footer-horizontal bg-base-200 text-base-content p-10">
-            <aside>
-                <img className='w-[50px] h-[50px]' src={logo} alt="" />
-                <p className='font-bold'>
-                    Volunteer Industries Ltd.
+        <footer className=" bg-base-200 text-base-content p-10">
+            <div className='max-w-6xl mx-auto footer  sm:footer-horizontal'>
+                <aside>
+                    <img className='w-[50px] h-[50px]' src={logo} alt="" />
+                    <p className='font-bold'>
+                        Volunteer Industries Ltd.
 
-                </p>
-            </aside>
-            <nav>
-                <h6 className="footer-title">Services</h6>
-                <Link to='/'>Home</Link>
-                <Link to='/addPost'>Add Post</Link>
-                <Link to='/allPost'>All Volunteer Need Posts</Link>
-            </nav>
-            <nav>
-                <h6 className="footer-title">Social</h6>
-                <Link to={'https://www.facebook.com/subrota.roy.97524/'} className="link link-hover">Facebook</Link>
-                <Link to={'https://x.com/Subrota0157'} className="link link-hover">Twetter</Link>
-                <Link to={'https://www.youtube.com/'} className="link link-hover">YouTube</Link>
-            </nav>
+                    </p>
+                </aside>
+                <nav>
+                    <h6 className="footer-title">Services</h6>
+                    <Link to='/'>Home</Link>
+                    <Link to='/addPost'>Add Post</Link>
+                    <Link to='/allPost'>All Volunteer Need Posts</Link>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Social</h6>
+                    <Link to={'https://www.facebook.com/subrota.roy.97524/'} className="link link-hover">Facebook</Link>
+                    <Link to={'https://x.com/Subrota0157'} className="link link-hover">Twetter</Link>
+                    <Link to={'https://www.youtube.com/'} className="link link-hover">YouTube</Link>
+                </nav>
+            </div>
         </footer>
     );
 };
